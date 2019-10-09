47ecb031ddec9626c892d371fa46e0d40ec32f76
atul511996 <atul511996@gmail.com>
2019-10-09 23:19:01 +0530

    done changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 8007
 
 const user = require("./route/user")
 const auth = require("./auth/auth")
-const home = require("./route/home")
+
 const db = require("./database/db")
 app.use(fileUpload());
 app.use(cors())
@@ -18,7 +18,7 @@ app.use("/user" , user)
 
 app.use("/api" , auth)
 
-app.use("/api/home" , home)
+
 
 app.listen(port , ()=>{
 })
--- socket/client.js
+++
@@ -1,6 +0,0 @@
-const io = require('socket.io-client')
-const ioClient = io.connect('http://localhost:8001' , {rejectUnauthorized : false})
-
-ioClient.on("7", (data)=>{
-    console.log(data)
-})
